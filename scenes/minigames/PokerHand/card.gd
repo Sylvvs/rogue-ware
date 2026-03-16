65b04df1bfc20aa8df176a9021a408b8dcdb856a
Ulveboss <mikkel@iploug.dk>
2026-03-16 11:20:54 +0100

    poker :3

---
+++ scenes/minigames/PokerHand/card.gd
@@ -0,0 +1,39 @@
+extends TextureButton
+
+@export var value_sheet: Texture2D
+@export var back_sheet: Texture2D
+@export var tile_size: Vector2i = Vector2i(71,95)
+
+var suit: int = 0
+var value: int = 0
+var index = 0
+
+var focused = false
+
+var tween = null
+@onready var value_sprite = $Value
+@onready var background_sprite = $Background
+
+func _ready():
+	update_sprite()
+
+func set_card(s, v):
+	suit = s
+	value = v
+	update_sprite()
+	
+func update_sprite():
+	if not value_sheet or not back_sheet:
+		return
+
+	# Card face (suit x value)
+	var face := AtlasTexture.new()
+	face.atlas = value_sheet
+	face.region = Rect2(Vector2(value * tile_size.x, suit * tile_size.y), tile_size)
+	value_sprite.texture = face
+
+	# Card back
+	var back := AtlasTexture.new()
+	back.atlas = back_sheet
+	back.region = Rect2(Vector2(71,0), tile_size)
+	background_sprite.texture = back
---
+++ scenes/minigames/PokerHand/poker_hand.gd
@@ -0,0 +1,50 @@
+extends Minigame
+
+@onready var card_scene = preload("res://scenes/minigames/PokerHand/card.tscn")
+@onready var container = $Container
+@onready var value_sheet = preload("res://images/DeckOfCards.png")
+@onready var back_sheet = preload("res://images/PC _ Computer - Balatro - Playing Cards - Card Backs, Enhancers and Seals.png")
+var hand_size = 6
+var rng = RandomNumberGenerator.new()
+var hand = []
+var hand_counter = 0
+var held = 0
+
+
+func start():
+	time_limit = 7
+	instruction_text = "Make the best hand with the following cards!"
+	
+	for i in range(hand_size):
+		var new_card = card_scene.instantiate()
+		container.add_child(new_card)
+
+		new_card.value_sheet = value_sheet
+		new_card.back_sheet = back_sheet
+
+		# Random card
+		new_card.suit = rng.randi_range(0,3)
+		new_card.value = rng.randi_range(0,12)
+		new_card.set_card(new_card.suit,new_card.value)
+		new_card.index = (13 * new_card.suit) + new_card.value
+		hand.append(new_card.index)
+		new_card.tween = null
+		new_card.pressed.connect(Callable(self, "on_button_pressed").bind(new_card))
+
+func on_button_pressed(button: TextureButton):
+	
+	if button.tween:
+		button.tween.kill()
+	button.tween = create_tween()
+		
+		
+	if held < 5 and !button.focused:
+		button.focused = !button.focused
+		button.tween.tween_property(button, "position", button.position + Vector2(0,-30), 0.1).set_ease(Tween.EASE_OUT)
+		held += 1
+	elif button.focused:
+		button.focused = false
+		button.tween.tween_property(button, "position", button.position + Vector2(0,30), 0.1).set_ease(Tween.EASE_OUT)
+		held -= 1 
+	
+	pass
