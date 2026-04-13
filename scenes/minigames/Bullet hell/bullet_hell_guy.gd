32ea2a3efead7eebc5d1a2c56e0257eed98a5645
Sylvvs <kongsted.simon@gmail.com>
2026-04-13 10:05:47 +0200

    bullet hell works

--- scenes/minigames/Bullet hell/bullet_hell_guy.gd
+++ scenes/minigames/Bullet hell/bullet_hell_guy.gd
@@ -8,7 +8,7 @@ const SPEED = 300.0
 
 
 func _physics_process(delta: float) -> void:
-	var direction = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
+	var direction = Vector2(Input.get_axis("walk_left", "walk_right"), Input.get_axis("up", "down"))
 	if direction != Vector2.ZERO:
 		velocity = direction.normalized() * SPEED
 		
