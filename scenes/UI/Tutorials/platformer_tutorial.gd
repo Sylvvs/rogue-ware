26a12b5383a98f32e37084f5cd837a1bda22a59a
Ulveboss <mikkel@iploug.dk>
2026-04-17 11:50:07 +0200

    tutorial

--- scenes/UI/Tutorials/platformer_tutorial.gd
+++ scenes/UI/Tutorials/platformer_tutorial.gd
@@ -7,5 +7,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 
 func _on_area_2d_2_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://scenes/UI/StartScreen/start_screen.tscn")
+	if body.is_in_group('player'):
+		get_tree().change_scene_to_file("res://scenes/UI/StartScreen/start_screen.tscn")
 	pass # Replace with function body.
