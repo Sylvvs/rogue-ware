5bf7729d21940d2f28920a2e1cf344f6addea778
Maffeq <Victor.scharf.heinemann@outlook.dk>
2026-04-17 17:10:16 +0200

    yuh

--- start_screen.gd
+++ start_screen.gd
@@ -9,23 +9,15 @@ extends Control
 @onready var background_high = $BackgroundHigh
 @onready var background_low = $BackgroundLow
 
-var background_on = false
 
 func _ready() -> void:
 	play_knap.text = "PLAY"
 	settings_knap.text = "SETTINGS"
 	quit_knap.text = "QUIT"
 	tutorials_knap.text = "TUTORIALS"
+	background_low.visible = true
+	background_high.visible = false	
 
-
-func _process(delta: float) -> void:
-	if background_on == true:
-		background_low.visible = false
-		background_high.visible = true
-	else: 
-		background_on = false
-		background_low.visible = true
-		background_high.visible = false
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/GameHandler.tscn")
@@ -47,8 +39,5 @@ func _on_tutorials_pressed() -> void:
 
 
 func _on_high_detail_toggled(toggled_on: bool) -> void:
-	if toggled_on:
-		background_on = true
-	else:
-		background_on = false
-	pass # Replace with function body.
+	background_high.visible = toggled_on
+	background_low.visible = not toggled_on
