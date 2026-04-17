e2f6455cb9457eaaabc6a29e840e3c066ab576cf
Maffeq <Victor.scharf.heinemann@outlook.dk>
2026-04-17 13:38:21 +0200

    background toggle :D

--- start_screen.gd
+++ start_screen.gd
@@ -5,6 +5,10 @@ extends Control
 @onready var settings_knap = $VBoxContainer/Settings
 @onready var quit_knap = $VBoxContainer/Quit
 @onready var tutorials_knap = $VBoxContainer/Tutorials
+@onready var high_detail = $HighDetail
+@onready var background = $Background
+
+var background_on = false
 
 func _ready() -> void:
 	play_knap.text = "PLAY"
@@ -13,6 +17,12 @@ func _ready() -> void:
 	tutorials_knap.text = "TUTORIALS"
 
 
+func _process(delta: float) -> void:
+	if background_on == true:
+		background.visible = true
+	else: 
+		background_on = false
+		background.visible = false
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/GameHandler.tscn")
@@ -30,4 +40,12 @@ func _on_quit_pressed() -> void:
 
 func _on_tutorials_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/UI/Tutorials/platformer_tutorial.tscn")
+	pass # Replace with function body.
+
+
+func _on_high_detail_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		background_on = true
+	else:
+		background_on = false
 	pass # Replace with function body.
