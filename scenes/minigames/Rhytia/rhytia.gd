233b5f16a5db4763f0559ac58f007a834a63b13d
Maffeq <Victor.scharf.heinemann@outlook.dk>
2026-04-22 14:38:58 +0200

    wow

--- scenes/minigames/Rhytia/rhytia.gd
+++ scenes/minigames/Rhytia/rhytia.gd
@@ -19,9 +19,9 @@ func start():
 	allowed_misses = allowed_misses - round(allowed_misses_scale * mult)
 	instruction_text = "Hit the block with your mouse " + "and don't miss more than:" + " " + str(allowed_misses)
 	time_limit = 20
-	time_limit = clamp(ceil(time_limit * mult),20,Conductor.get_song_length())
+	time_limit = clamp(ceil(time_limit * mult),20,300)
 	if full_song:
-		time_limit = Conductor.get_song_length()
+		time_limit = 300
 	if song != "":
 		get_parent().get_parent().music.play_specific_track(song)
 	
@@ -40,10 +40,6 @@ func _process(delta: float) -> void:
 	accuracy_text.text = "Accuracy " + "\n" + str(snapped(Conductor.get_accuracy(), 0.1)) + "%"
 	multiplier_text.text = str(Conductor.multiplier) + "x"
 	if Conductor.notes_missed >= allowed_misses:
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		Conductor.stop()
-		lose()
-	if Conductor.notes_missed >= 10 and mult == 1.05:
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		Conductor.stop()
 		lose()
