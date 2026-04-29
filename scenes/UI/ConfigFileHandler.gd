9bb6325eb5a6c2b70882645c1b8e71f7148b772b
Ulveboss <mikkel@iploug.dk>
2026-04-29 14:10:52 +0200

    everything works

--- scenes/UI/ConfigFileHandler.gd
+++ scenes/UI/ConfigFileHandler.gd
@@ -17,12 +17,28 @@ func _ready():
 		config.save(SETTINGS_FILE_PATH)
 	else:
 		config.load(SETTINGS_FILE_PATH)
-
+	
+	_apply_saved_volumes()
+	
 	var keybindings = load_keybindings()
 	for action in keybindings.keys():
 		InputMap.action_erase_events(action)
 		InputMap.action_add_event(action, keybindings[action])
 
+func _apply_saved_volumes() -> void:
+	_apply_volume("Master", load_volume("master", 0.5))
+	_apply_volume("Music",  load_volume("music",  0.5))
+	_apply_volume("SFX",    load_volume("sfx",    0.5))
+
+func _apply_volume(bus_name: String, value: float) -> void:
+	var bus_index = AudioServer.get_bus_index(bus_name)
+	if bus_index == -1:
+		return
+	if value == 0.0:
+		AudioServer.set_bus_mute(bus_index, true)
+	else:
+		AudioServer.set_bus_mute(bus_index, false)
+		AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
 
 func save_keybinding(action: StringName, event: InputEvent):
 	var event_str
