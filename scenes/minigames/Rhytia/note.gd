2d0bc357f1646ab0822c7d4876b878b0f37f8274
Sylvvs <kongsted.simon@gmail.com>
2026-04-29 14:03:07 +0200

    audio works yayyyy

--- scenes/minigames/Rhytia/note.gd
+++ scenes/minigames/Rhytia/note.gd
@@ -72,8 +72,8 @@ func register_hit(rating: String):
 	var i = Conductor.mult_levels.find(Conductor.multiplier)
 	has_been_hit = true
 	perfect_hit_sound.reparent(get_tree().current_scene)
-	perfect_hit_sound.play()
 	perfect_hit_sound.bus = "SFX"
+	perfect_hit_sound.play()
 	Conductor.combo += 1
 	Conductor.successful_hits += 1
 	Conductor.total_notes += 1
